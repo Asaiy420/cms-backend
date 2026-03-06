90a50bbb1280ac5d00aa8513ed64e9a783594925
Asaiy420 <kcaryan05@gmail.com>
2026-03-06 20:37:34 +0545

    feat: set up CORS middleware and add subjects API route

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import express, { type Request, type Response } from 'express';
+import subjectsRouter from './routes/subject';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT!;
 
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL!,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true, // Allow cookies to be sent with requests
+  })
+);
 app.use(express.json());
+
+app.use('/api/subjects', subjectsRouter);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, World!');
