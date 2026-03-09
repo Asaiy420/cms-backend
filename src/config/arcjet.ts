45b279cb20a3cd66769f30c467cf56e42007a2eb
Asaiy420 <kcaryan05@gmail.com>
2026-03-09 10:14:50 +0545

    added arcjet for security and created schema types for authentication

---
+++ src/config/arcjet.ts
@@ -0,0 +1,25 @@
+import arcjet, { shield, detectBot, slidingWindow } from '@arcjet/node';
+
+const aj = arcjet({
+    key: process.env.ARCJET_KEY!,
+    rules: [
+        // Shield protects app from common attacks e.g. SQL injection
+        shield({ mode: 'LIVE' }),
+        // Create a bot detection rule
+        detectBot({
+            mode: 'LIVE', // Blocks requests. Use "DRY_RUN" to log only
+            // Block all bots except the following
+            allow: [
+                'CATEGORY:SEARCH_ENGINE', // Google, Bing, etc
+                'CATEGORY:PREVIEW',
+            ],
+        }),
+        slidingWindow({
+            mode: 'LIVE',
+            interval: '2m',
+            max: 5,
+        }),
+    ],
+});
+
+export default aj;
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,27 @@
 import express, { type Request, type Response } from 'express';
 import subjectsRouter from './routes/subject';
 import cors from 'cors';
+import securityMiddleware from './middleware/security';
 
 const app = express();
 const PORT = process.env.PORT!;
 
 app.use(
-  cors({
-    origin: process.env.FRONTEND_URL!,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true, // Allow cookies to be sent with requests
-  })
+    cors({
+        origin: process.env.FRONTEND_URL!,
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        credentials: true, // Allow cookies to be sent with requests
+    })
 );
 app.use(express.json());
+app.use(securityMiddleware);
 
 app.use('/api/subjects', subjectsRouter);
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, World!');
+    res.send('Hello, World!');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
