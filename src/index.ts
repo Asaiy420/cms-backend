4fee01f9a0c130fa8feb662300c5199864aaf3c8
Asaiy420 <kcaryan05@gmail.com>
2026-03-16 16:22:20 +0545

    refactor: improve code formatting and indentation in index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,11 @@ const app = express();
 const PORT = process.env.PORT!;
 
 app.use(
-    cors({
-        origin: process.env.FRONTEND_URL!,
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        credentials: true, // Allow cookies to be sent with requests
-    })
+  cors({
+    origin: process.env.FRONTEND_URL!,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true, // Allow cookies to be sent with requests
+  })
 );
 
 app.all('/api/auth/*splat', toNodeHandler(auth));
@@ -24,9 +24,9 @@ app.use(securityMiddleware);
 app.use('/api/subjects', subjectsRouter);
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, World!');
+  res.send('Hello, World!');
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
