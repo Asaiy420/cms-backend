d276cfd9c72a20b40115f33ed91fc3f9896410eb
Asaiy420 <kcaryan05@gmail.com>
2026-03-22 11:24:56 +0545

    refactor: improve code formatting and indentation in auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,28 +4,28 @@ import { db } from '../db';
 import * as schema from '../db/schema/auth';
 
 export const auth = betterAuth({
-    secret: process.env.BETTER_AUTH_SECRET!,
-    trustedOrigins: [process.env.FRONTEND_URL!],
-    database: drizzleAdapter(db, {
-        provider: 'pg', // or "mysql", "sqlite"
-        schema,
-    }),
-    emailAndPassword: {
-        enabled: true,
+  secret: process.env.BETTER_AUTH_SECRET!,
+  trustedOrigins: [process.env.FRONTEND_URL!],
+  database: drizzleAdapter(db, {
+    provider: 'pg', // or "mysql", "sqlite"
+    schema,
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  user: {
+    additionalFields: {
+      role: {
+        type: 'string',
+        required: true,
+        defaultValue: 'student',
+        input: true,
+      },
+      imageCldPubId: {
+        type: 'string',
+        required: false,
+        input: true,
+      },
     },
-    user: {
-        additionalFields: {
-            role: {
-                type: 'string',
-                required: true,
-                defaultValue: 'student',
-                input: true,
-            },
-            imageCldPubId: {
-                type: 'string',
-                required: false,
-                input: true,
-            },
-        },
-    },
+  },
 });
